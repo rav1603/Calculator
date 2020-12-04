841ad3398a22c73c40b38e8e82eb815de70b1ab1
Rafał Łukowski <rafal.j.lukowski@gmail.com>
2020-12-05 00:21:11 +0100

    ParameterBuilder for REST API number generator

---
+++ src/test/java/pl/calculator/client/ParameterBuilderTest.java
@@ -0,0 +1,27 @@
+package pl.calculator.client;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.Map;
+
+class ParameterBuilderTest {
+
+    ParameterBuilder parameterBuilder = new ParameterBuilder();
+
+    @Test
+    void shouldReturnStringContainingAllParametersGivenInMap() {
+        Map<String, String> params = Map.of("param1", "1", "param2", "2");
+        String result = parameterBuilder.getParamsString(params);
+
+        Assertions.assertThat(result).contains("param2=2").contains("param1=1");
+    }
+
+    @Test
+    void shouldReturnEmptyStringWhenEmptyMapIsGiven() {
+        String result = parameterBuilder.getParamsString(Collections.emptyMap());
+
+        Assertions.assertThat(result).isEmpty();
+    }
+}
